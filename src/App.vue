46e71d234c0d79a76b388d56d1e411291616dd95
SelimF1998 <mohamedselim.feroukhi@enicar.ucar.tn>
2022-12-21 11:52:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid items-center justify-center">
+    <Todos />
+  </div>
+</template>
+<script>
+import Sidebar from './components/Sidebar.vue'
+import Todos from './components/Todos.vue'
+
+export default {
+  name: "app",
+  components: {
+    Sidebar,
+    Todos
+  },
+  methods: {
+    search(val) { 
+    }
+  }
+};
+</script>
+<style></style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 class="text-red-500 font-bold text-3xl mt-5 text-center">Todos</h1>
+    <div class="todos flex items-center justify-center flex-wrap gap-5 mt-5">
+      <div
+        v-for="todo in allTodos"
+        :key="todo.id"
+        class="card w-96 bg-base-100 shadow-xl"
+      >
+        <div class="card-body">
+          <h2 class="card-title">{{ todo.title }}</h2>
+          <p>{{ todo.description }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import {mapGetters, mapActions} from "vuex";
+
+export default {
+  name: "Todos",
+  methods: {
+    ...mapActions(["fetchTodos"])
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos()
+  }
+};
+</script>
+<style></style>
